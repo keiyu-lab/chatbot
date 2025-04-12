81004d8f169e4f8979a3c705651568b8162c56ba
keiyu-lab <82919564+keiyu-lab@users.noreply.github.com>
2025-04-12 11:03:40 +0900

    コメントアウト清書

--- chatbot_project/init_chatbot/create_embddings.py
+++ chatbot_project/init_chatbot/create_embddings.py
@@ -1,3 +1,9 @@
+""" 
+    set up file
+    if database for cache already exists, don't need to run this file
+    when you want to change the source given to chatbot, change the url
+"""
+
 import os
 import openai
 
@@ -9,10 +15,6 @@ from langchain.document_loaders import WebBaseLoader, TextLoader
 openai.api_key = os.environ["OPENAI_API_KEY"]
 CHROMA_DB_DIRECTORY="chroma_db/opneAI"
 
-# set up file
-# if database for chatbot has already, don't need to run this file
-# but when you want to change the source given to chatbot, change the url
- 
 def build_database(urls, collection_name):
     loader = WebBaseLoader(urls)
     documents = loader.load()
@@ -26,9 +28,9 @@ def build_database(urls, collection_name):
     splitted_additinal_documents = text_splitter.split_documents(additional_document)
 
     # add Document to list(Document)
-    # note:splitted_additional_documents is list(Document)
+    # splitted_additional_documents is list(Document)
     splitted_documents.append(splitted_additinal_documents[0])
-    #print(splitted_documents)
+
     embeddings = OpenAIEmbeddings()
        
     # .from_documents is more suitable for searching information than from_texts 
@@ -44,9 +46,9 @@ def build_database(urls, collection_name):
     
     return vectordb
         
-# for example, I scrape openAi info from wiki
-urls_OpenPIM = [
+# default url 
+url = [
        "https://en.wikipedia.org/wiki/OpenAI"
 ]
-vectordb=build_database(urls_OpenPIM, "opneAI")
+vectordb=build_database(url, "opneAI")
  
